72ef0a142674c3660d072d33bc9c169238ecacec
ChristopherKuan <89368950+ChristopherKuan@users.noreply.github.com>
2024-09-02 11:58:06 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -53,8 +53,8 @@ def move_robot():
                         pid_left = PID(kp, ki, kd, output_limits=(0,1), starting_output=left_speed)
                         pid_right = PID(kp, ki, kd, output_limits=(0,1), starting_output=right_speed)
                         while left_encoder.value < 20:
-                            left_speed = pid_left(left_speed)
-                            right_speed = pid_right(right_speed)
+                            left_speed = pid_left(left_encoder.value)
+                            right_speed = pid_right(right_encoder.value)
                             pibot.value = (left_speed, right_speed)
                             print(left_encoder.value, right_encoder.value)
                         # if left_speed < right_speed: # turn left
@@ -91,8 +91,7 @@ def move_robot():
                     #     print(left_encoder.value, right_encoder.value)
                     
                     pibot.value = (left_speed, right_speed)
-                    print(left_encoder.value)
-                    print(right_encoder.value)
+                    print(left_encoder.value, right_encoder.value)
                     left_encoder.reset()
                     right_encoder.reset()
                     flag_new_pid_cycle = True          
