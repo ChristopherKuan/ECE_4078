8878d867868d3cf6d85ad631da8e975a29bf83af
ChristopherKuan <89368950+ChristopherKuan@users.noreply.github.com>
2024-09-02 09:24:47 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -73,20 +73,21 @@ def move_robot():
                         right_encoder.reset()
                         flag_new_pid_cycle = True
                 else:
-                    # if motion == 'stop':
-                    #     pibot.value = (left_speed, right_speed) 
-                    #     left_encoder.reset()
-                    #     right_encoder.reset()
-                    #     flag_new_pid_cycle = True
-                    # elif motion == 'turning':
-                    #     pibot.value = (left_speed, right_speed)
-                    #     print(left_encoder.value, right_encoder.value)
-                    pibot.value = (left_speed, right_speed)
-                    print(left_encoder.value)
-                    print(right_encoder.value)
-                    left_encoder.reset()
-                    right_encoder.reset()
-                    flag_new_pid_cycle = True          
+                    if motion == 'stop':
+                        pibot.value = (left_speed, right_speed) 
+                        left_encoder.reset()
+                        right_encoder.reset()
+                        flag_new_pid_cycle = True
+                    elif motion == 'turning':
+                        pibot.value = (left_speed, right_speed)
+                        print(left_encoder.value, right_encoder.value)
+                        flag_new_pid_cycle = True  
+                    # pibot.value = (left_speed, right_speed)
+                    # print(left_encoder.value)
+                    # print(right_encoder.value)
+                    # left_encoder.reset()
+                    # right_encoder.reset()
+                    # flag_new_pid_cycle = True          
             else:
                 left_speed, right_speed = abs(left_speed), abs(right_speed)
                 if flag_new_pid_cycle:
