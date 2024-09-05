274e2e9d10e24c6a0ac7977aee85dcf82705e408
dwij100 <dulhanwijeyeweere@gmail.com>
2024-09-05 21:18:31 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -129,7 +129,7 @@ def move_robot():
                     else:
                         pibot.value = (-left_speed, -right_speed)
                 print(left_encoder.value - right_encoder.value)
-        time.sleep(0.05)
+        time.sleep(0.1)
     
     
 # Receive confirmation whether to use pid or not to control the wheels (forward & backward)
