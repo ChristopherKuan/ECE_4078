3c2bc4f52ed55024ec813641847356a2012b4ecb
dwij100 <dulhanwijeyeweere@gmail.com>
2024-09-07 10:56:05 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -50,12 +50,12 @@ def move_robot():
                     if left_speed > right_speed:
                         dir = "right"
                         if flag_new_pid_cycle:
-                            pid_left = PID(kp, ki, kd, setpoint=right_encoder.value, output_limits=(-1,1), starting_output=left_speed)
+                            pid_left = PID(kp, ki, kd, setpoint=right_encoder.value, output_limits=(0,1), starting_output=left_speed)
                             flag_new_pid_cycle = False
                     else:
                         dir = "left"
                         if flag_new_pid_cycle:
-                            pid_right = PID(kp, ki, kd, setpoint=left_encoder.value, output_limits=(-1,1), starting_output=right_speed)
+                            pid_right = PID(kp, ki, kd, setpoint=left_encoder.value, output_limits=(0,1), starting_output=right_speed)
                             flag_new_pid_cycle = False
                     if dir == "left":
                         pid_right.setpoint = left_encoder.value
