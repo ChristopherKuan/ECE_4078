39770e12ad7b6de1c203d848f15be17cbf481a26
ChristopherKuan <89368950+ChristopherKuan@users.noreply.github.com>
2024-09-12 21:37:40 +0800

    Update listenhao.py

--- listenhao.py
+++ listenhao.py
@@ -30,7 +30,7 @@ class Encoder(object):
 
 # main function to control the robot wheels
 def move_robot():
-    global use_pid, left_speed, right_speed
+    global use_pid, left_speed, right_speed, motion
     flag_new_pid_cycle = True
     while True:
         if milestone != 4:
